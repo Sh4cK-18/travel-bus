de3ae92565c9f61a1461786e2cb816051e2f3aad
Sh4cK-18 <auuz.451@gmail.com>
2024-07-08 20:13:48 -0500

    chore: Add endpoints to UserController for fetching users and user by ID

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Get('get-users')
+    async getAllUsers() {
+        return await this.userService.getUsers();
+    }
+
+    @Get('get-user/:id')
+    async getUserById(@Param('id') id: string) {
+        return await this.userService.getUserById(id);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prisma: PrismaService) {}
+
+    async getUsers() {
+        try {
+            const data = await this.prisma.usuario.findMany({
+                select: {
+                    usuarioId: true,
+                    nombre: true,
+                    apellido: true,
+                    email: true,
+                    createdAt: true,
+                    updatedAt: true
+                }
+            })
+            if(!data) {
+              throw new HttpException(
+                {
+                    messageError: 'Data not found',
+                },
+                HttpStatus.NOT_FOUND
+              )
+            }
+            return {message: 'Data fetched successfully', data};
+
+        } catch (error) {
+           return {message: 'Error fetching data', error};
+        }
+    }
+    async getUserById(id: string) {
+        try {
+            const data = await this.prisma.usuario.findUnique({
+                where: {
+                    usuarioId: Number(id)
+                },
+                select: {
+                    usuarioId: true,
+                    nombre: true,
+                    apellido: true,
+                    email: true,
+                    createdAt: true,
+                    updatedAt: true
+                }
+            })
+            if(!data) {
+              throw new HttpException(
+                {
+                    messageError: 'Data not found',
+                },
+                HttpStatus.NOT_FOUND
+              )
+            }
+            return {message: 'Data fetched successfully', data};
+
+        } catch (error) {
+           return {message: 'Error fetching data', error};
+        }
+    }
+}
