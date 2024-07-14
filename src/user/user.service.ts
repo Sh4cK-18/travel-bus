b8360bd2a8ab4f2510d39cdd20079806e4c46347
Sh4cK-18 <auuz.451@gmail.com>
2024-07-14 13:23:26 -0500

    chore: Remove duplicate import in user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,4 @@
 import { BadRequestException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { BadRequestException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 
