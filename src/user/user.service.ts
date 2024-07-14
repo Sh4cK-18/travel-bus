7f78de77539b7695bb2c4fa5a05753bc51b8c5e9
Sh4cK-18 <auuz.451@gmail.com>
2024-07-14 11:20:02 -0500

    chore: Add updateUser endpoint to UserController for updating user information

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -58,6 +59,77 @@ export class UserService {
 
         } catch (error) {
            return {message: 'Error fetching data', error};
+        }
+    }
+
+    async updateUser(id: string, updateData: { nombre?: string; apellido?: string; password?: string; roles?: string[] }) {
+        try {
+            if (updateData.password) {
+                updateData.password = await bcrypt.hash(updateData.password, 10);
+            }
+
+            let rolesArray = [];
+            if (updateData.roles) {
+                if (updateData.roles.length === 0) {
+                    const defaultRole = await this.prisma.rol.findFirst({
+                        where: {
+                            name: 'USUARIO',
+                        },
+                    });
+                    rolesArray.push({ rolId: defaultRole.rolId });
+                } else {
+                    const provideRole = await this.prisma.rol.findMany({
+                        where: {
+                            name: {
+                                in: updateData.roles,
+                            },
+                        },
+                    });
+                    if (provideRole.length > 0) {
+                        rolesArray = provideRole.map(role => ({ rolId: role.rolId }));
+                    } else {
+                        throw new BadRequestException('One or more roles do not exist');
+                    }
+                }
+            }
+
+            const data = await this.prisma.usuario.update({
+                where: {
+                    usuarioId: Number(id)
+                },
+                data: {
+                    ...updateData,
+                    roles: {
+                        set: rolesArray,
+                    },
+                },
+                select: {
+                    usuarioId: true,
+                    nombre: true,
+                    apellido: true,
+                    email: true,
+                    roles: {
+                        select: {
+                            rol: {
+                                select: {
+                                    name: true
+                                }
+                            }
+                        }
+                    },
+                    createdAt: true,
+                    updatedAt: true
+                }
+            });
+            return { message: 'User updated successfully', data };
+        } catch (error) {
+            throw new HttpException(
+                {
+                    messageError: 'Error updating user',
+                    error: error.message,
+                },
+                HttpStatus.BAD_REQUEST
+            );
         }
     }
 }
